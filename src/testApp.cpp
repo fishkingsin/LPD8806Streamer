88d3021a6f1b515a6c531eeab93b09b2e5f47327
James Kong <fishkingsin@gmail.com>
2012-12-03 10:21:40 +0800

    update

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,6 +1,10 @@
 #include "testApp.h"
 float pos_x = 0;
-int numLED = 64;
+int row = 1;
+int col = 64;
+int numLED = col*row;
+int SQAURE_ROOT =int(sqrt(col));
+float ptSize = 5;
 //--------------------------------------------------------------
 void testApp::setup(){
 	
@@ -9,11 +13,14 @@ void testApp::setup(){
 	led = new ofxLEDsLPD8806(numLED);
 	for(int i = 0 ; i< numLED ; i++)
 	{
-		led->addLED(i,ofVec2f(50+20+(i%8)*40,	100+20+(i/8)*40));
+		float x = 50+20+(i%col)*ptSize;
+		float y = 100+20+(i/col)*ptSize;
+		led->addLED(i,ofVec2f(x,y));
 	}
 	ofSetFrameRate(25);
 	
-	serial.setup("/dev/tty.usbmodem5d11", 115200);
+	serial.listDevices();
+	serial.setup(serial.getDeviceList()[5].getDeviceName(), 115200);
 	
 	ofSetLogLevel(OF_LOG_NOTICE);
 	mode = 1;
@@ -38,8 +45,9 @@ void testApp::update(){
 	//[8 px first row ][8 px second row ]
 	drawGraphic();
 	led->renderBuffer.end();
-	
+
 	led->encode();
+	
 	serial.writeBytes((unsigned char*)led->txBuffer.data(), led->txBuffer.size());
 	
 }
@@ -48,29 +56,29 @@ void testApp::drawGraphic()
 	switch(mode)
 	{
 		case 1:
-			for(int i = 0 ; i < 8 ; i++)
+			for(int i = 0 ; i < SQAURE_ROOT ; i++)
 			{
-				player.draw(i*8,-i, 8, 8);
+				player.draw(i*SQAURE_ROOT,-i, SQAURE_ROOT, SQAURE_ROOT);
 			}
 			break;
 		case 2:
 		{
 			ofPushStyle();
 			ofEnableBlendMode(OF_BLENDMODE_ALPHA);
-			int step = 360/16.0;
-			for(int i = 0 ; i < 16 ; i++)
+			int step = 360.0/SQAURE_ROOT;
+			for(int i = 0 ; i < 1 ; i++)
 			{
 				ofSetColor(ofColor::fromHsb((i*step)%360, 255, 255),200);
-				ofCircle(int(pos_x+i*4-16)%64, 0, 3);
+				ofCircle(int(pos_x+i*(SQAURE_ROOT)-16)%numLED, 0, SQAURE_ROOT*0.5);
 			}
 			ofDisableBlendMode();
 			ofPopStyle();
 		}
 			break;
 			case 3 :
-			for(int i = 0 ; i < 8 ; i++)
+			for(int i = 0 ; i < SQAURE_ROOT ; i++)
 			{
-				grabber.draw(i*8,-i, 8, 8);
+				grabber.draw(i*SQAURE_ROOT,-i, SQAURE_ROOT, SQAURE_ROOT);
 			}
 
 			break;
@@ -83,16 +91,16 @@ void testApp::draw(){
 	ofScale(10, 10);
 	//draw again to preview
 	drawGraphic();
-	led->encodedBuffer.draw(0,0);
+//	led->encodedBuffer.draw(0,0);
 	ofPopMatrix();
 	
 	ofPushStyle();
 	ofNoFill();
 	ofSetColor(0, 255, 0);
-	ofRect(0,0,640,10);
+	ofRect(0,0,numLED*10,10);
 	ofPopStyle();
 	
-	led->draw();
+	led->draw(ptSize);
 	
 	//led->draw();
 	
